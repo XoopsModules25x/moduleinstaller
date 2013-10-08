b712b9b9f50a12db0ddf5ab91578db029201a68d
Mamba <mambax7@gmail.com>
2013-10-08 03:25:49 -0400

    Adding ModuleInstaller 1.0 RC 1

---
+++ moduleinstaller/docs/readme.txt
@@ -0,0 +1,35 @@
+README
+===========
+
+ModuleInstaller is a XOOPS module to install and uninstall XOOPS Modules in bulk, instead of doing it one by one. 
+
+ Requirements
+ _____________________________________________________________________
+ 
+- XOOPS >= 2.5.0
+- PHP version >= 5.2.0
+- ModuleClasses in /Frameworks (download it from here: http://goo.gl/Bmknt)  
+
+Install/uninstall
+---------------------
+No special measures necessary, follow the standard installation process – extract the module folder into the ../modules directory. Install the module through Admin -> System Module -> Modules.
+
+Detailed instructions on installing modules are available in the XOOPS Operations Manual: http://goo.gl/adT2i
+
+Operating instructions
+----------------------
+This module and its operations are very simple:
+
+i) select the tab for desired operation: install or unistall modules
+
+ii) select modules
+
+iii) click on Next button
+
+iv) Review the messages for errors, and click on Next Button
+
+
+Tutorial
+-----------
+
+At this moment there is no tutorial available
---
+++ moduleinstaller/include/install_tpl.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+/**
+ * Installer template file
+ *
+ * See the enclosed file license.txt for licensing information.
+ * If you did not receive this file, get it at http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * @copyright   The XOOPS project http://www.xoops.org/
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @package     installer
+ * @since       2.3.0
+ * @author      Haruki Setoyama  <haruki@planewave.org>
+ * @author      Kazumi Ono <webmaster@myweb.ne.jp>
+ * @author      Skalpa Keo <skalpa@xoops.org>
+ * @author      Taiwen Jiang <phppp@users.sourceforge.net>
+ * @author      Kris <kris@frxoops.org>
+ * @author      DuGris (aka L. JEN) <dugris@frxoops.org>
+ * @version     $Id: install_tpl.php 10042 2012-08-08 22:34:35Z beckmi $
+**/
+
+defined('XOOPS_INSTALL') or die('XOOPS Installation wizard die');
+
+include_once '../../../language/' . $wizard->language . '/global.php';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo _LANGCODE; ?>" lang="<?php echo _LANGCODE; ?>">
+
+<head>
+    <title>
+        <?php echo XOOPS_VERSION . ' : ' . XOOPS_INSTALL_WIZARD; ?>
+        (<?php echo ($wizard->pageIndex + 1) . '/' . count($wizard->pages); ?>)
+    </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=<?php echo _INSTALL_CHARSET ?>" />
+    <link rel="shortcut icon" type="image/ico" href="../favicon.ico" />
+    <link charset="UTF-8" rel="stylesheet" type="text/css" media="all" href="css/style.css" />
+    <?php
+        if (file_exists('language/' . $wizard->language . '/style.css')) {
+            echo '<link charset="UTF-8" rel="stylesheet" type="text/css" media="all" href="language/' . $wizard->language . '/style.css" />';
+        } else {
+		$xoTheme->addStylesheet( XOOPS_URL . "/modules/" . $xoopsModule->getVar("dirname") . "/css/style.css" );
+		}
+    ?>
+    
+    <script type="text/javascript" src="./js/prototype-1.6.0.3.js"></script>
+    <script type="text/javascript" src="./js/xo-installer.js"></script>
+</head>
+
+<body>
+
+    <div id="xo-content">
+
+
+                    <form id='<?php echo $wizard->pages[$wizard->currentPage]['name']; ?>' action='<?php echo $_SERVER['PHP_SELF']; ?>' method='post'>
+
+                            <?php echo $content; ?>
+
+                        <div id="buttons">
+                            <?php if (@$pageHasForm) { ?>
+                                <button type="submit">
+                            <?php } else { ?>
+                                <button type="button" accesskey="n" onclick="location.href='<?php echo 'index.php'; ?>'">
+                            <?php } ?>
+                            <?php echo BUTTON_NEXT; ?>
+                            </button>
+                        </div>
+                    </form>
+
+    </div>
+</body>
+</html>
